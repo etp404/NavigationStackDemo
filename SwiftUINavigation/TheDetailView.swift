f3bb468f33a98218af5892c073a04903faea93fd
Matt Mould <etp404@users.noreply.github.com>
2022-06-10 13:42:42 +0100

    First commit

---
+++ SwiftUINavigation/TheDetailView.swift
@@ -0,0 +1,16 @@
+//
+//  TheDetailView.swift
+//  SwiftUINavigation
+//
+//  Created by Matthew Mould on 10/06/2022.
+//
+
+import Foundation
+import SwiftUI
+
+struct TheDetailView: View {
+    let itemLabel: String
+    var body: some View {
+        Text("This is the detail view for \(itemLabel)")
+    }
+}
